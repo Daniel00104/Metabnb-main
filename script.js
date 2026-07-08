9d3c7dc6db284224857e7ab2a01d9f925e4652f3
Okeke Chukwuemeka Daniel <danielokeke2004@gmail.com>
2026-07-08 17:44:50 +0100

    new changes

---
+++ script.js
@@ -0,0 +1,11 @@
+const openBtn = document.getElementById('menu-open-button');
+const closeBtn = document.getElementById('menu-close-button');
+const navMenu = document.getElementById('nav-menu');
+
+openBtn.addEventListener('click', () => {
+    navMenu.classList.add('open');
+});
+
+closeBtn.addEventListener('click', () => {
+    navMenu.classList.remove('open');
+});
